80fd05fdeffd9ab2ae71448daa65225552825d15
Autio <petri.mikael.autio@gmail.com>
2020-09-24 23:25:51 +0300

    Implemented growth sounds. Added GUI button library

--- Assets/scripts/MushroomSpawn.cs
+++ Assets/scripts/MushroomSpawn.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 using DG.Tweening;
 public class MushroomSpawn : MonoBehaviour
 {
+    public AudioClip[] growSounds;
+
     float originalScale;
     float startTimer = 0f;
     public Transform[] spawnEffects;
@@ -16,7 +18,7 @@ public class MushroomSpawn : MonoBehaviour
         this.GetComponent<Transform>().localScale = new Vector3(0.01f, 0.01f, 0.01f);
 
         GetComponent<SpriteRenderer>().enabled = false;
-        startTimer = Random.Range(0.1f, 1.2f);
+        startTimer = Random.Range(0.1f, 1.3f);
         spawnEffects[0].GetComponent<ParticleSystem>().GetComponent<Renderer>().sortingOrder = this.GetComponent<Renderer>().sortingOrder + 1;  // Front effect
         spawnEffects[1].GetComponent<ParticleSystem>().GetComponent<Renderer>().sortingOrder = this.GetComponent<Renderer>().sortingOrder - 1;  // Back effect
     }
@@ -24,8 +26,8 @@ public class MushroomSpawn : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-
-            startTimer -= Time.deltaTime;
+        
+        startTimer -= Time.deltaTime;
         
         if (startTimer < 0 && !spawned)
         {
@@ -44,51 +46,33 @@ public class MushroomSpawn : MonoBehaviour
 
     public void SpawnMushroom(float finalScale = 1.0f)
     {
+
         StartCoroutine(PlayParticles(.1f));
+        StartCoroutine(PlaySound(.7f));
 
         Sequence verticalMove = DOTween.Sequence();
 
-        //verticalMove.Append(this.GetComponent<Transform>().DOLocalMoveY(transform.localPosition.y - 1f, 0.01f));
         verticalMove.Append(this.GetComponent<Transform>().DOLocalMoveY(transform.localPosition.y + .25f, 2.7f));
 
         Sequence widthSequence = DOTween.Sequence();
         Sequence heightSequence = DOTween.Sequence();
-        // Assume that the size that the 
-        float originalScale = this.GetComponent<Transform>().localScale.x;
-        // Make sure it starts small
-        // Random time before the mushroom starts growing 
-        //  widthSequence.Append(this.GetComponent<Transform>().DOScaleX(0.01f, Random.Range(0.1f, .2f)));
 
-
-        // Make the mushroom grow up
+        // Delay the start a bit so that the particles can roam for a while
         heightSequence.AppendInterval(.7f);
+        
+                // Make the mushroom grow up
         heightSequence.Append(this.GetComponent<Transform>().DOScale(finalScale / 100, .1f));
-
         heightSequence.Append(this.GetComponent<Transform>().DOScale(finalScale / 4, .2f));
 
         heightSequence.Append(this.GetComponent<Transform>().DOScale(finalScale * Random.Range(1.2f, 1.4f), .6f));
-        heightSequence.Append(this.GetComponent<Transform>().DOScale(finalScale / 1.5f, .4f));
-        //heightSequence.Append(this.GetComponent<Transform>().DOScale(finalScale / 4, .4f));
 
+        heightSequence.Append(this.GetComponent<Transform>().DOScale(finalScale / 1.5f, .4f));
         heightSequence.Append(this.GetComponent<Transform>().DOScale(finalScale, .35f));
 
         // Grow in a pulse-like manner, always heading towards the target scale
         float shrinkTime = Random.Range(.10f, .20f);
         float xGrowTime = Random.Range(.3f, .5f);
-
-        //widthSequence.Append(this.GetComponent<Transform>().DOScaleX(originalScale / 4, .1f));
-
-        //widthSequence.Append(this.GetComponent<Transform>().DOScaleX(originalScale * .8f, xGrowTime));
-        //widthSequence.Append(this.GetComponent<Transform>().DOScaleX(originalScale / 4, shrinkTime));
-
-        //widthSequence.Append(this.GetComponent<Transform>().DOScaleX(originalScale * 1.2f, xGrowTime));
-        //widthSequence.Append(this.GetComponent<Transform>().DOScaleX(originalScale / 2, shrinkTime));
-
-        //widthSequence.Append(this.GetComponent<Transform>().DOScaleX(originalScale * Random.Range(1.5f, 2f), xGrowTime + .2f));
-
-        //// Final Scale
-        //widthSequence.Append(this.GetComponent<Transform>().DOScaleX(finalScale, shrinkTime));
-        
+       
     }
 
     private IEnumerator PlayParticles(float delay)
@@ -99,5 +83,14 @@ public class MushroomSpawn : MonoBehaviour
         {
             effect.GetComponent<ParticleSystem>().Play();
         }
+    }
+
+    private IEnumerator PlaySound(float delay)
+    {
+        yield return new WaitForSeconds(delay);
+        // Play sound
+        
+        GetComponent<AudioSource>().clip = growSounds[Random.Range(0, growSounds.Length)];
+        GetComponent<AudioSource>().Play();
     }
 }
